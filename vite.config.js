31d8b14214ff15a12f3ee4aec607e0db5d11ddf6
manya1604 <manyamangla16@gmail.com>
2025-03-26 02:29:11 +0530

    Fixed vite.config.js for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Manya-Portfolio/", // Ensure this matches your GitHub repo name
 })
